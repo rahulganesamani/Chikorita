f4fbac11baf1570678cb3d4fffd3f26383f88a92
Rahul <134198813+rahulganesamani@users.noreply.github.com>
2024-08-03 20:47:15 +0530

    Create LC_2024_08_03.cpp

---
+++ LC_2024_08_03.cpp
@@ -0,0 +1,14 @@
+// 1460. Make Two Arrays Equal by Reversing Subarrays
+
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        for ( int i = 0; i < arr.size(); i++ ) {
+            if ( arr[i] != target[i] )
+                return false;
+        }
+        return true;
+    }
+};
